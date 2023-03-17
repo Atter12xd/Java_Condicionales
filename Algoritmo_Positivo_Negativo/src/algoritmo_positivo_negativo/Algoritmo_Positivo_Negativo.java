0010b80c3143a7d89f5f4f8d151f34a35841fe3d
Atter12xd <1485275@senati.pe>
2023-03-17 10:52:06 -0500

    Importamos la libreria, llamamos a la clase y creamos un String

---
+++ Algoritmo_Positivo_Negativo/src/algoritmo_positivo_negativo/Algoritmo_Positivo_Negativo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package algoritmo_positivo_negativo;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Algoritmo_Positivo_Negativo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
