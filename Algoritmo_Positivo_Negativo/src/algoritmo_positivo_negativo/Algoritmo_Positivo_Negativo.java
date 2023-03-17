344fd096b4a7feccecaee833ecf54ec02cbf6cec
Atter12xd <1485275@senati.pe>
2023-03-17 10:58:14 -0500

    creamos un int, despues lo leemos y usamos las condicionales que pertenecen

--- Algoritmo_Positivo_Negativo/src/algoritmo_positivo_negativo/Algoritmo_Positivo_Negativo.java
+++ Algoritmo_Positivo_Negativo/src/algoritmo_positivo_negativo/Algoritmo_Positivo_Negativo.java
@@ -9,6 +9,17 @@ public class Algoritmo_Positivo_Negativo {
         System.out.println("Ingrese su nombre: "); // pedimos al usario que ingrese su nombre
         nombre = dato.nextLine(); // Leemos la cadena String
         System.out.println("Hola! " + nombre); // Saludamos y llamamos a la variable nombre
+        System.out.println("Ponga su numero: "); // Pedimos al usario que ingrese su numero
+        int num = dato.nextInt(); // leemos entero usando nextInt
+        if (num > 0){ // SI num es mayor que 0 imprimira lo siguiente
+            System.out.println("Hola : " + nombre + " " +  "Su numero " + num +  " es positivo! ");
+        }
+        else if (num == 0){ //Si num es igual que 0 imprimira lo siguiente
+            System.out.println("Hola : " + nombre + " " +  "Su numero 0 no es negativo y tampoco es positivo ");
+        } 
+        else{ // De lo contrario a todas las opciones imprima lo siguiente
+            System.out.println("Hola : " + nombre + " " +  "Su numero " + num + " es negativo! ");
+        }
 
         
     }
