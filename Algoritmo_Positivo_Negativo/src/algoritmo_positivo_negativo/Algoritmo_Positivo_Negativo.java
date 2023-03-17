5a6795ab11bfb702eed5bddc14a9174c9b14eb29
Atter12xd <1485275@senati.pe>
2023-03-17 10:55:33 -0500

    pedimos al usario que ingresa su nombre y leemos su cadena con nextLine

--- Algoritmo_Positivo_Negativo/src/algoritmo_positivo_negativo/Algoritmo_Positivo_Negativo.java
+++ Algoritmo_Positivo_Negativo/src/algoritmo_positivo_negativo/Algoritmo_Positivo_Negativo.java
@@ -6,7 +6,8 @@ public class Algoritmo_Positivo_Negativo {
     public static void main(String[] args) {
         Scanner dato = new Scanner(System.in); // llamamos a Scanner
         String nombre = ""; // Creamos un String 
-        
+        System.out.println("Ingrese su nombre: "); // pedimos al usario que ingrese su nombre
+        nombre = dato.nextLine(); // Leemos la cadena String
 
         
     }
