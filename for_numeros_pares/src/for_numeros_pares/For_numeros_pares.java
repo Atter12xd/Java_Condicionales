0828d5b4f66973a4e770a677619d4983e0457a37
Atter12xd <1485275@senati.pe>
2023-03-18 10:54:48 -0500

    Importando Scanner y hacemos los pedidos de entrada para el usario

---
+++ for_numeros_pares/src/for_numeros_pares/For_numeros_pares.java
@@ -0,0 +1,18 @@
+
+package for_numeros_pares;
+
+import java.util.Scanner;
+public class For_numeros_pares {
+
+    
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digame su nombre: ");
+        String nombre = entrada.nextLine();
+        System.out.println("Ingrese su numero");
+        int IngresoNumero = entrada.nextInt();
+
+        
+    }
+    
+}
