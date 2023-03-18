47f5efa69ad81b69f6160e69f28362b0ad212efe
Atter12xd <1485275@senati.pe>
2023-03-18 10:56:43 -0500

    Ahora usamos el for y ponemos en dos en dos que seria +=2

--- for_numeros_pares/src/for_numeros_pares/For_numeros_pares.java
+++ for_numeros_pares/src/for_numeros_pares/For_numeros_pares.java
@@ -11,6 +11,13 @@ public class For_numeros_pares {
         String nombre = entrada.nextLine();
         System.out.println("Ingrese su numero");
         int IngresoNumero = entrada.nextInt();
+        int suma = 0;
+        for (int numero = 1; numero <= IngresoNumero ; numero +=2 ){
+            suma = suma + numero;
+        }
+
+    System.out.println("hola " + nombre + " la suma de los numeros pares es: " + suma);
+
 
         
     }
