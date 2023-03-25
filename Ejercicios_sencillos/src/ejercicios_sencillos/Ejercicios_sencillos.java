9de6bacc16a2c768b2b96c68520129a8eac352d3
Atter12xd <1485275@senati.pe>
2023-03-25 08:16:10 -0500

    Trabajito de imprimir palabras como esta en la pizarra

---
+++ Ejercicios_sencillos/src/ejercicios_sencillos/Ejercicios_sencillos.java
@@ -0,0 +1,18 @@
+
+package ejercicios_sencillos;
+
+public class Ejercicios_sencillos {
+
+    public static void main(String[] args) {
+        System.out.println("Morning, Morning, Morning");
+        System.out.println("        Hello, Have a good day");
+        System.out.println("                 oh! Thank you");
+        System.out.println("                              ");
+        System.out.println("                              ");
+        System.out.println("                 You are great");
+        System.out.println("        This is my pleasure  ");
+        System.out.println("No,... You are welcome");
+
+    }
+
+}
--- Ingreso_de_dos_numeros/src/ingreso_de_dos_numeros/Ingreso_de_dos_numeros.java
+++ Ingreso_de_dos_numeros/src/ingreso_de_dos_numeros/Ingreso_de_dos_numeros.java
@@ -16,15 +16,19 @@ public class Ingreso_de_dos_numeros {
         int numero2 = ingreso.nextInt();
         if (numero1 != numero2) { // si el primer numero es diferente al segundo numero imprime lo siguiente
             System.out.println(numero1 + " es diferente a " + numero2);
+
         }
         if (numero1 < numero2) { // si el primer numero es menor al segundo numero imprimer lo siguiente
             System.out.println(numero1 + " es menor a " + numero2);
+            System.out.println(numero2 + " es mayor a " + numero1);
+
         }
         if (numero1 <= numero2) { // si el primer numero es menor o igual al siguiente numero imprime lo siguiente
             System.out.println(numero1 + " es menor o igual a " + numero2);
         }
         if (numero1 > numero2) { // si el primer numero es mayor al segundo numero imprimero lo siguiente
             System.out.println(numero1 + " es mayor a " + numero2);
+            System.out.println(numero2 + " es menor a " + numero1);
         }
 
     }
